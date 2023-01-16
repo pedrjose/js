1dae1c572bf9b8cf91c12c3ec178d21b1ef3cdbb
pedro josé lima <pedrlimadev@gmail.com>
2023-01-16 12:11:46 -0300

    ex8 finished

--- exercises/ex8/ex8.js
+++ exercises/ex8/ex8.js
@@ -1,30 +1,31 @@
 // Write a JavaScript program to calculate days left until next Christmas and show it in a HTML page.
 
-let day = new Date(); let today = day.getDate(); let month = day.getMonth() + 1;
-let daysUntilChristmas = 359; let counterDays = 0; let counterMonths = 0;
+function daysUntilChristmas() {
+    let day = new Date(); let today = day.getDate(); let month = day.getMonth() + 1; 
+    let counterMonths = 0; let daysUntilChristmas = 359; let counterDays = 0; 
 
-while (counterDays != today && counterMonths != month) {
-    if (counterMonths <= 4) {
-        counterDays += 1;
-        if (counterDays == 30) {
-            counterMonths += 1;
+    while (counterDays != today && counterMonths != month) {
+        if (counterMonths <= 4) {
+            counterDays += 1;
+            if (counterDays == 30) {
+                counterMonths += 1;
+            }
+        }
+        else if (counterMonths > 4 && counterMonths % 2 != 0 && counterMonths < 12) {
+            counterDays += 1;
+            if (counterDays == 31) {
+                counterMonths += 1;
+            }
+        }
+        else if (counterMonths > 4 && counterMonths % 2 == 0 && counterMonths < 12) {
+            counterDays += 1;
+            if (counterDays == 30) {
+                counterMonths += 1;
+            }
+        }
+        else {
+            counterDays += 25;
         }
     }
-    else if (counterMonths > 4 && counterMonths % 2 != 0 && counterMonths < 12) {
-        counterDays += 1;
-        if (counterDays == 31) {
-            counterMonths += 1;
-        }
-    }
-    else if (counterMonths > 4 && counterMonths % 2 == 0 && counterMonths < 12) {
-        counterDays += 1;
-        if (counterDays == 30) {
-            counterMonths += 1;
-        }
-    }
-    else {
-        counterDays += 25;
-    }
+    document.getElementById("result").innerHTML = daysUntilChristmas - counterDays;
 }
-
-console.log(`\n${daysUntilChristmas-counterDays} DAYS UNTIL NEXT CHRISTMAS!!`);
