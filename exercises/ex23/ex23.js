ab226752606ed96c64a0e4fe7162cb3c97dfb382
pedro josé lima <pedrlimadev@gmail.com>
2023-02-01 17:17:36 -0300

    new exercises

---
+++ exercises/ex23/ex23.js
@@ -0,0 +1,6 @@
+const inputText = document.querySelector('.input-text');
+const inputButton = document.querySelector('.input-button');
+
+inputButton.addEventListener('click', function () {
+    alert('PIKA');
+})
